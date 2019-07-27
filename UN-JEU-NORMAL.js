516c7c7050131ce53c487175eb00a2f51c62d60a
YOLO R <52312596+haydenledean@users.noreply.github.com>
2019-07-27 17:50:05 +0200

    Add files via upload

---
+++ UN-JEU-NORMAL.js
@@ -0,0 +1,30 @@
+console.warn("Stop tu n'a rien a faire ici sauf si tu es un devloppeur. Merci !")
+alert('Bienvenue sur un site web tout a fait normal !');
+alert('Moi je suis ALERTO !');
+alert('Et...');
+alert('...');
+alert('Tu peux partir il y a rien a faire ici !');
+alert('Tu ne veux pas ?');
+alert('Moi je te dit a demain !');
+alert('ZZZ');
+alert('ZZZ');
+alert('ZZZ');
+alert('ZZZ');
+alert('ZZZ');
+alert('Tu es encore sur mon site ?');
+alert('Part je te dit !!!');
+alert('Il y a rien ici !!!');
+alert('ALERT : FORTNITE VEUX JOUER AVEC TOI !');
+alert('ALERT : TON AMI SUR FACEBOOK VEUX TE PARLER !');
+alert('ALERT : PENSE A ALLER AU TRAVAIL PLUTÔT QUE DE ME PARLER !');
+alert('Ok tu a gagné je vais partir 1 heure');
+alert('');
+alert('');
+alert('');
+alert('');
+alert('');
+alert('');
+alert('');
+alert('Quoi tu es encore ici ?');
+alert('ⓉⓊ ⓋⒶ ⓅⒶⓇⓉⒾⓇ ⓄⓊⒾ !!!')
+//Le texte ne s'affiche pas sur Sublime Text mais ça marche quand même.
